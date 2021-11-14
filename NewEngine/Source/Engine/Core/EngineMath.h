0960967b75f1343c499b408e65238f777b0be7c0
Nicolas Risberg <nicolas.risberg@gmail.com>
2021-11-14 19:31:46 +0100

    changed const refs in math file to copies

--- NewEngine/Source/Engine/Core/EngineMath.h
+++ NewEngine/Source/Engine/Core/EngineMath.h
@@ -18,39 +18,39 @@ namespace NewEngine
 		static const F32 TauReciprocal;
 
 		template<typename T>
-		static inline T Sqrt(const T& x);
+		static inline T Sqrt(T x);
 		template<typename T>
-		static inline T Square(const T& x);
+		static inline T Square(T x);
 
 		/** Checks if x is within the range, exclusive on max. */
 		template<typename T>
-		static inline bool IsWithin(const T& x, const T& minValue, const T& maxValue);
+		static inline bool IsWithin(T x, T minValue, T maxValue);
 		
 		/** Clamps x to be between minValue and maxValue, inclusive on max. */
 		template<typename T>
-		static inline T Clamp(const T& x, const T& minValue = 0, const T& maxValue = 1);
+		static inline T Clamp(T x, T minValue = 0, T maxValue = 1);
 
 		template<typename T>
-		static inline T Abs(const T& x);
+		static inline T Abs(T x);
 
 		template<typename T>
-		static inline T Lerp(const T& a, const T& b, const T& t);
+		static inline T Lerp(T a, T b, T t);
 		template<typename T>
-		static inline T InverseLerp(const T& a, const T& b, const T& t);
+		static inline T InverseLerp(T a, T b, T t);
 
 		/** Remaps value from inRange to outRange. */
 		template<typename T>
-		static inline T Remap(const T& inRangeMin, const T& inRangeMax, const T& outRangeMin, const T& outRangeMax, const T& value);
+		static inline T Remap(T inRangeMin, T inRangeMax, T outRangeMin, T outRangeMax, T value);
 
 		template<typename T>
-		static inline T Sin(const T& angle);
+		static inline T Sin(T angle);
 		template<typename T>
-		static inline T Cos(const T& angle);
+		static inline T Cos(T angle);
 
 		template<typename T>
-		static inline T DegToRad(const T& angleInDegrees);
+		static inline T DegToRad(T angleInDegrees);
 		template<typename T>
-		static inline T RadToDeg(const T& angleInRadians);
+		static inline T RadToDeg(T angleInRadians);
 	};
 
 	const F32 UMath::Pi = 3.14159265359f;
@@ -60,74 +60,74 @@ namespace NewEngine
 	const F32 UMath::TauReciprocal = 0.15915494309f;
 
 	template<typename T>
-	inline T UMath::Sqrt(const T& x)
+	inline T UMath::Sqrt(T x)
 	{
 		return std::sqrt(x);
 	}
 
 	template<typename T>
-	inline T UMath::Square(const T& x)
+	inline T UMath::Square(T x)
 	{
 		return x * x;
 	}
 
 	template<typename T>
-	inline bool UMath::IsWithin(const T& x, const T& minValue, const T& maxValue)
+	inline bool UMath::IsWithin(T x, T minValue, T maxValue)
 	{
 		return ((x >= minValue) && (x < maxValue));
 	}
 
 	template<typename T>
-	inline T UMath::Clamp(const T& x, const T& minValue, const T& maxValue)
+	inline T UMath::Clamp(T x, T minValue, T maxValue)
 	{
 		return x < minValue ? minValue : x < maxValue ? x : maxValue;
 	}
 
 	template<typename T>
-	inline T UMath::Abs(const T& x)
+	inline T UMath::Abs(T x)
 	{
 		return std::abs(x);
 	}
 
 	template<typename T>
-	inline T UMath::Lerp(const T& a, const T& b, const T& t)
+	inline T UMath::Lerp(T a, T b, T t)
 	{
 		return a * (1 - t) + (b * t);
 	}
 	
 	template<typename T>
-	inline T UMath::InverseLerp(const T& a, const T& b, const T& t)
+	inline T UMath::InverseLerp(T a, T b, T t)
 	{
 		return (t - a) / (b - a);
 	}
 
 	template<typename T>
-	inline T UMath::Remap(const T& inRangeMin, const T& inRangeMax, const T& outRangeMin, const T& outRangeMax, const T& value)
+	inline T UMath::Remap(T inRangeMin, T inRangeMax, T outRangeMin, T outRangeMax, T value)
 	{
 		T t = InverseLerp(inRangeMin, inRangeMax, value);
 		return Lerp(outRangeMin, outRangeMax, t);
 	}
 
 	template<typename T>
-	inline T UMath::Sin(const T& angle)
+	inline T UMath::Sin(T angle)
 	{
 		return std::sin(angle);
 	}
 
 	template<typename T>
-	inline T UMath::Cos(const T& angle)
+	inline T UMath::Cos(T angle)
 	{
 		return std::cos(angle);
 	}
 
 	template<typename T>
-	inline T UMath::DegToRad(const T& angleInDegrees)
+	inline T UMath::DegToRad(T angleInDegrees)
 	{
 		return angleInDegrees * (UMath::Pi * DEGREES_TO_RADIANS_RECIPROCAL);
 	}
 
 	template<typename T>
-	inline T UMath::RadToDeg(const T& angleInRadians)
+	inline T UMath::RadToDeg(T angleInRadians)
 	{
 		return angleInRadians * (180 * UMath::PiReciprocal);
 	}
