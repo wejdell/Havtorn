ff91f67e7015a14767ee3ce1b0c0a12f6bdff14c
Nicolas Risberg <nicolas.risberg@gmail.com>
2021-11-24 12:59:26 +0100

    Added catmull rom 3d curve

--- Havtorn/Source/Engine/Core/MathTypes/Curve.h
+++ Havtorn/Source/Engine/Core/MathTypes/Curve.h
@@ -6,24 +6,42 @@ namespace Havtorn
 {
 	struct ICurve
 	{
-		virtual SVector GetPoint(float t) = 0;
+		virtual SVector GetPoint(F32 t) = 0;
+		virtual SVector GetTangent(F32 t) = 0;
+		virtual SVector GetNormal(F32 t) = 0;
 	};
 
 	struct SQuadraticBezier : public ICurve
 	{
-		SVector GetPoint(float t) override
+		SVector GetPoint(F32 t) override
 		{
 			F32 tt = t * t;
 			
 			return
 				P2 * (tt)
-		      + P1 * (2*t - tt)
-			  + P0 * (1 - 2*t + tt)
+		      + P1 * (2.0f*t - tt)
+			  + P0 * (1.0f - 2.0f*t + tt)
 			;
 		}
 
-		SQuadraticBezier(const SVector& p0, const SVector& p1, const SVector& p2)
-			: P0(p0), P1(p1), P2(p2)
+		SVector GetTangent(F32 t) override
+		{
+			return
+				P2 * (2.0f*t)
+			  + P1 * (2.0f - 2.0f*t)
+			  + P0 * (-2.0f + 2.0f*t)
+			;
+		}
+
+		SVector GetNormal(F32 t) override
+		{
+			SVector tangent = GetTangent(t);
+			// TODO.NR: Rotate tangent 90 degrees for normal
+			return tangent;
+		}
+
+		explicit SQuadraticBezier(const SVector& p0, const SVector& p1, const SVector& p2)
+			: P0(p0), P1(p1), P2(p2) 
 		{}
 
 		SVector P0, P1, P2;
@@ -31,20 +49,82 @@ namespace Havtorn
 
 	struct SCubicBezier : public ICurve
 	{
-		SVector GetPoint(float t) override
+		SVector GetPoint(F32 t) override
 		{
 			F32 tt = t * t;
 			F32 ttt = t * t * t;
 
 			return
 				P3 * (ttt)
-			  + P2 * (3*tt - 3*ttt)
-			  + P1 * (3*t - 6*tt + 3*ttt)
-			  + P0 * (1 - 3*t + 3*tt - ttt)
+			  + P2 * (3.0f*tt - 3.0f*ttt)
+			  + P1 * (3.0f*t - 6.0f*tt + 3.0f*ttt)
+			  + P0 * (1.0f - 3.0f*t + 3.0f*tt - ttt)
 			;
 		}
 
-		SCubicBezier(const SVector& p0, const SVector& p1, const SVector& p2, const SVector& p3)
+		SVector GetTangent(F32 t)
+		{
+			F32 tt = t * t;
+
+			return
+				P3 * (3.0f*tt)
+			  + P2 * (6.0f*t - 9.0f*tt)
+			  + P1 * (3.0f - 12.0f*t + 9.0f*tt)
+			  + P0 * (-3.0f + 6.0f*t - 3.0f*tt)
+				;
+		}
+
+		SVector GetNormal(F32 t) override
+		{
+			SVector tangent = GetTangent(t);
+			// TODO.NR: Rotate tangent 90 degrees for normal
+			return tangent;
+		}
+
+		explicit SCubicBezier(const SVector& p0, const SVector& p1, const SVector& p2, const SVector& p3)
+			: P0(p0), P1(p1), P2(p2), P3(p3)
+		{}
+
+		SVector P0, P1, P2, P3;
+	};
+
+	struct SCatmullRom : public ICurve
+	{
+		SVector GetPoint(F32 t) override
+		{
+			F32 tt = t * t;
+			F32 ttt = t * t * t;
+
+			return
+				0.5f *
+			   (P3 * (-tt + ttt)
+			  + P2 * (t + 4.0f*tt - 3.0f*ttt)
+			  + P1 * (2.0f - 5.0f*tt + 3.0f*ttt)
+			  + P0 * (-t + 2.0f*tt - ttt))
+				;
+		}
+
+		SVector GetTangent(F32 t)
+		{
+			F32 tt = t * t;
+
+			return
+				0.5f *
+			   (P3 * (-2.0f*t + 3.0f*tt)
+			  + P2 * (1.0f + 8.0f*t - 9.0f*tt)
+			  + P1 * (-10.0f*t + 9.0f*tt)
+			  + P0 * (-1.0f + 4.0f*t - 3*tt))
+				;
+		}
+
+		SVector GetNormal(F32 t) override
+		{
+			SVector tangent = GetTangent(t);
+			// TODO.NR: Rotate tangent 90 degrees for normal
+			return tangent;
+		}
+
+		explicit SCatmullRom(const SVector& p0, const SVector& p1, const SVector& p2, const SVector& p3)
 			: P0(p0), P1(p1), P2(p2), P3(p3)
 		{}
 
