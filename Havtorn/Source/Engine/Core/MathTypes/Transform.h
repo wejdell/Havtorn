14408752279aab8dd180fe253cd5728cd0856476
Nicolas Risberg <nicolas.risberg@gmail.com>
2022-09-05 21:36:18 +0200

    Math fixes, added scaling, renamed getters and setters

--- Havtorn/Source/Engine/Core/MathTypes/Transform.h
+++ Havtorn/Source/Engine/Core/MathTypes/Transform.h
@@ -23,6 +23,14 @@ namespace Havtorn
 		inline void Rotate(const SVector& eulerAngles);
 		inline void Translate(const SVector& v);
 		inline void Translate(const SVector4& v);
+		inline void Move(const SVector& v);
+		inline void Move(const SVector4& v);
+		// N.B: Multiplicative scaling
+		inline void Scale(const SVector& v);
+		// N.B: Multiplicative scaling
+		inline void Scale(F32 xScale, F32 yScale, F32 zScale);
+		// N.B: Multiplicative scaling
+		inline void Scale(F32 scale);
 		inline void Orbit(const STransform& transform, const SMatrix& rotation);
 		inline void Orbit(const SVector& point, const SMatrix& rotation);
 		inline void Orbit(const SVector4& point, const SMatrix& rotation);
@@ -69,9 +77,9 @@ namespace Havtorn
 		if (eulerAngles.IsEqual(SVector::Zero))
 			return;
 
-		const SMatrix rightRotation = SMatrix::CreateRotationAroundAxis(eulerAngles.X, LocalMatrix.Right());
-		const SMatrix upRotation = SMatrix::CreateRotationAroundAxis(eulerAngles.Y, LocalMatrix.Up());
-		const SMatrix forwardRotation = SMatrix::CreateRotationAroundAxis(eulerAngles.Z, LocalMatrix.Forward());
+		const SMatrix rightRotation = SMatrix::CreateRotationAroundAxis(eulerAngles.X, LocalMatrix.GetRight());
+		const SMatrix upRotation = SMatrix::CreateRotationAroundAxis(eulerAngles.Y, LocalMatrix.GetUp());
+		const SMatrix forwardRotation = SMatrix::CreateRotationAroundAxis(eulerAngles.Z, LocalMatrix.GetForward());
 
 		SMatrix finalRotation = LocalMatrix.GetRotationMatrix();
 		finalRotation *= rightRotation;
@@ -82,15 +90,15 @@ namespace Havtorn
 
 	inline void STransform::Translate(const SVector& v)
 	{
-		SVector localMove = LocalMatrix.Right() * v.X;
+		SVector localMove = LocalMatrix.GetRight() * v.X;
 		LocalMatrix.M[3][0] += localMove.X;
 		LocalMatrix.M[3][1] += localMove.Y;
 		LocalMatrix.M[3][2] += localMove.Z;
-		localMove = LocalMatrix.Up() * v.Y;
+		localMove = LocalMatrix.GetUp() * v.Y;
 		LocalMatrix.M[3][0] += localMove.X;
 		LocalMatrix.M[3][1] += localMove.Y;
 		LocalMatrix.M[3][2] += localMove.Z;
-		localMove = LocalMatrix.Forward() * v.Z;
+		localMove = LocalMatrix.GetForward() * v.Z;
 		LocalMatrix.M[3][0] += localMove.X;
 		LocalMatrix.M[3][1] += localMove.Y;
 		LocalMatrix.M[3][2] += localMove.Z;
@@ -98,18 +106,39 @@ namespace Havtorn
 
 	inline void STransform::Translate(const SVector4& v)
 	{
-		SVector localMove = LocalMatrix.Right() * v.X;
-		LocalMatrix.M[3][0] += localMove.X;
-		LocalMatrix.M[3][1] += localMove.Y;
-		LocalMatrix.M[3][2] += localMove.Z;
-		localMove = LocalMatrix.Up() * v.Y;
-		LocalMatrix.M[3][0] += localMove.X;
-		LocalMatrix.M[3][1] += localMove.Y;
-		LocalMatrix.M[3][2] += localMove.Z;
-		localMove = LocalMatrix.Forward() * v.Z;
-		LocalMatrix.M[3][0] += localMove.X;
-		LocalMatrix.M[3][1] += localMove.Y;
-		LocalMatrix.M[3][2] += localMove.Z;
+		Translate({ v.X, v.Y, v.Z });
+	}
+
+	inline void STransform::Move(const SVector& v)
+	{
+		Translate(v);
+	}
+
+	inline void STransform::Move(const SVector4& v)
+	{
+		Translate(v);
+	}
+
+	inline void STransform::Scale(const SVector& scale)
+	{
+		F32 validScale[3];
+		validScale[0] = scale.X < FLT_EPSILON ? 0.001f : scale.X;
+		validScale[1] = scale.Y < FLT_EPSILON ? 0.001f : scale.Y;
+		validScale[2] = scale.Z < FLT_EPSILON ? 0.001f : scale.Z;
+
+		LocalMatrix(0, 0) *= validScale[0];
+		LocalMatrix(1, 1) *= validScale[1];
+		LocalMatrix(2, 2) *= validScale[2];
+	}
+
+	inline void STransform::Scale(F32 xScale, F32 yScale, F32 zScale)
+	{
+		Scale({ xScale, yScale, zScale });
+	}
+
+	inline void STransform::Scale(F32 scale)
+	{
+		Scale({ scale, scale, scale });
 	}
 
 	// TODO.NR: Make transform struct which can store local transform data and make parent from point argument
