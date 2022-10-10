bc9fbc5753f8f94bd77bd08ac5d30c1c88669859
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2022-10-10 13:15:59 +0200

    Colorlist Updated

--- Havtorn/Source/Engine/Core/ColorList.h
+++ Havtorn/Source/Engine/Core/ColorList.h
@@ -6,9 +6,10 @@ namespace Havtorn
 {
 	namespace Color
 	{
-		const SVector4 Red =	{ 255.0f, 0.0f, 0.0f, 255.0f };
-		const SVector4 Green =	{ 0.0f, 255.0f, 0.0f, 255.0f };
-		const SVector4 Blue =	{ 0.0f,	0.0f, 255.0f, 255.0f };
-		const SVector4 White =	{ 255.0f, 255.0f, 255.0f, 255.0f };
+		const SVector4 Red =	{ 255.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f};
+		//const SVector4 Red =	{ 255.0f, 0.0f, 0.0f, 255.0f};
+		const SVector4 Green =	{ 0.0f / 255.0f, 255.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f };
+		const SVector4 Blue =	{ 0.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f };
+		const SVector4 White =	{ 255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f};
 	}
 }
