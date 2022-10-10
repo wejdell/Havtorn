1c8718965f021501f83d94a4418fe7851fc38a59
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2022-10-10 20:18:14 +0200

    Added newline to end of file. Removed comment

--- Havtorn/Source/Engine/Core/ColorList.h
+++ Havtorn/Source/Engine/Core/ColorList.h
@@ -7,7 +7,6 @@ namespace Havtorn
 	namespace Color
 	{
 		const SVector4 Red =	{ 255.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f};
-		//const SVector4 Red =	{ 255.0f, 0.0f, 0.0f, 255.0f};
 		const SVector4 Green =	{ 0.0f / 255.0f, 255.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f };
 		const SVector4 Blue =	{ 0.0f / 255.0f, 0.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f };
 		const SVector4 White =	{ 255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f};
