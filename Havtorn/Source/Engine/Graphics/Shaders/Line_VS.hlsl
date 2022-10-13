bec6a8ac4c952001a7953bdcedb3a0fd27cc282d
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2022-10-13 14:16:31 +0200

    White space end of file

--- Havtorn/Source/Engine/Graphics/Shaders/Line_VS.hlsl
+++ Havtorn/Source/Engine/Graphics/Shaders/Line_VS.hlsl
@@ -8,9 +8,10 @@ LineVertexToPixel main(LineVertexInput input)
     float4 vertexWorldPos       = mul(ToWorld, vertexObjectPos);
     float4 vertexViewPos        = mul(ToCameraSpace, vertexWorldPos);
     float4 vertexProjectionPos  = mul(ToProjectionSpace, vertexViewPos);
-
+    
     LineVertexToPixel returnValue;
     returnValue.Position = vertexProjectionPos;
     returnValue.Color = Color;
     return returnValue;
 }
+
