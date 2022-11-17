8ee87c4e202b7a29641caefd99f7c41266ddc646
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2022-11-17 15:32:57 +0100

    Updated color presets. Fixed warnings in constructor

--- Havtorn/Source/Engine/Core/Color.cpp
+++ Havtorn/Source/Engine/Core/Color.cpp
@@ -9,11 +9,11 @@ namespace Havtorn
 	 const SColor SColor::Red = SColor(255, 0, 0);
 	 const SColor SColor::Green = SColor(0, 255, 0);
 	 const SColor SColor::Blue = SColor(0, 0, 255);
-	 const SColor SColor::Black = SColor(0, 0, 0);
-	 const SColor SColor::White = SColor(255, 255, 255);
-	 const SColor SColor::Grey = SColor(123, 123, 123);
+	 const SColor SColor::Black = SColor(0);
+	 const SColor SColor::White = SColor(255);
+	 const SColor SColor::Grey = SColor(127);
 	 const SColor SColor::Teal = SColor(50, 255, 255);
-	 const SColor SColor::Orange = SColor(255, 135, 15);
-	 const SColor SColor::Purple = SColor(255, 25, 255);
-	 const SColor SColor::Yellow = SColor(255, 255, 25);
+	 const SColor SColor::Orange = SColor(255, 125, 15);
+	 const SColor SColor::Magenta = SColor(200, 0, 225);
+	 const SColor SColor::Yellow = SColor(255, 225, 25);
 }
