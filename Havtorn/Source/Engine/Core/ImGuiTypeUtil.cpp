53f76b1965f7758b02b0171e3716d8b30cf60107
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2022-11-23 17:56:53 +0100

    Added ImVec to SVector functions

--- Havtorn/Source/Engine/Core/ImGuiTypeUtil.cpp
+++ Havtorn/Source/Engine/Core/ImGuiTypeUtil.cpp
@@ -16,9 +16,21 @@ namespace Havtorn
 	{
 		return ImVec4( v.X, v.Y, v.Z, w );
 	}
-
 	inline ImVec4 UImGuiTypeUtil::Vector4ToImVec4(const SVector4& v)
 	{
 		return ImVec4( v.X, v.Y, v.Z, v.W );
+	}
+
+	inline SVector2<F32> UImGuiTypeUtil::ImVec2ToVector2(const ImVec2& v)
+	{
+		return SVector2<F32>(v.x, v.y);
+	}
+	inline SVector UImGuiTypeUtil::ImVec4ToVector(const ImVec4& v)
+	{
+		return SVector(v.x, v.y, v.z);
+	}
+	inline SVector4 UImGuiTypeUtil::ImVec4ToVector4(const ImVec4& v)
+	{
+		return SVector4(v.x, v.y, v.z, v.w);
 	}
 }
--- Havtorn/Source/Engine/Core/ImGuiTypeUtil.h
+++ Havtorn/Source/Engine/Core/ImGuiTypeUtil.h
@@ -13,5 +13,10 @@ namespace Havtorn
 		inline static ImVec2 Vector2ToImVec2(const SVector2<F32>& v);
 		inline static ImVec4 VectorToImVec4(const SVector& v, const F32 w);
 		inline static ImVec4 Vector4ToImVec4(const SVector4& v);
+
+	public:
+		inline static SVector2<F32> ImVec2ToVector2(const ImVec2& v);
+		inline static SVector ImVec4ToVector(const ImVec4& v);
+		inline static SVector4 ImVec4ToVector4(const ImVec4& v);
 	};
 }
