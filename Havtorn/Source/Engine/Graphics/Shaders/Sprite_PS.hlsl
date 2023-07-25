815b63f4af148588360ab27376a53482da323ce1
Nicolas R <nicolas.risberg@gmail.com>
2023-07-25 15:08:43 +0200

    Fixed sprite gamma

--- Havtorn/Source/Engine/Graphics/Shaders/Sprite_PS.hlsl
+++ Havtorn/Source/Engine/Graphics/Shaders/Sprite_PS.hlsl
@@ -1,11 +1,13 @@
 // Copyright 2023 Team Havtorn. All Rights Reserved.
 
 #include "Includes/SpriteShaderStructs.hlsli"
+#include "Includes/MathHelpers.hlsli"
 
 PixelOutput main(GeometryToPixel input)
 {
     PixelOutput output;
     float4 textureColor = spriteTexture.Sample(defaultSampler, input.UV.xy).rgba;
-    output.Color.rgba = textureColor.rgba * input.Color.rgba;
+    output.Color.rgb = GammaToLinear(textureColor.rgb) * GammaToLinear(input.Color.rgb);
+    output.Color.a = textureColor.a * input.Color.a;
     return output;
 }
