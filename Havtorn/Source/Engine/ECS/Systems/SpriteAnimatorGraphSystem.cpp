9146d9f5b6def87231355a9af6b336581f518b29
Axel Savage <axelsavage@gmail.com>
2023-08-05 20:02:15 +0200

    First iteration on Ticking an AnimationClip [WIP]

--- Havtorn/Source/Engine/ECS/Systems/SpriteAnimatorGraphSystem.cpp
+++ Havtorn/Source/Engine/ECS/Systems/SpriteAnimatorGraphSystem.cpp
@@ -5,6 +5,8 @@
 #include "hvpch.h"
 #include "SpriteAnimatorGraphSystem.h"
 #include "ECS/ComponentS/SpriteAnimatorGraphComponent.h"
+#include "ECS/ComponentS/SpriteComponent.h"
+
 
 namespace Havtorn
 {
@@ -18,10 +20,30 @@ namespace Havtorn
 
 	void CSpriteAnimatorGraphSystem::Update(CScene* scene)
 	{
-		std::vector<SSpriteAnimatorGraphComponent>& cmp = scene->GetSpriteAnimatorGraphComponents();
-		for (U64 i = 0; i < cmp.size(); i++)
+		std::vector<SSpriteAnimatorGraphComponent>& spriteAnimatorGraphComponents = scene->GetSpriteAnimatorGraphComponents();
+		std::vector<SSpriteComponent>& spriteComponents = scene->GetSpriteComponents();
+		const F32 deltaTime = GTime::Dt();
+		for (U64 i = 0; i < spriteAnimatorGraphComponents.size(); i++)
 		{
+			if (!spriteAnimatorGraphComponents[i].IsInUse)
+				continue;
 
+			spriteComponents[i].UVRect = TickAnimationClip(spriteAnimatorGraphComponents[i], deltaTime);
 		}
+	}
+
+	//Gotta add This ComponentData to Inspector types in the Editor Stuffs..!
+
+	SVector4 CSpriteAnimatorGraphSystem::TickAnimationClip(SSpriteAnimatorGraphComponent& c, const F32 deltaTime)
+	{
+		F32 duration = c.AnimationClips[c.CurrentAnimationClipKey].Durations[c.CurrentFrame];
+		c.Time += deltaTime;
+		if (c.Time >= duration)
+		{
+			U32 frameCount = static_cast<U32>(c.AnimationClips[c.CurrentAnimationClipKey].Durations.size());
+			c.CurrentFrame = (c.CurrentFrame + 1) % frameCount;
+			c.Time = 0.0f;
+		}
+		return c.AnimationClips[c.CurrentAnimationClipKey].UVRects[c.CurrentFrame];
 	}
 }
--- Havtorn/Source/Engine/ECS/Systems/SpriteAnimatorGraphSystem.h
+++ Havtorn/Source/Engine/ECS/Systems/SpriteAnimatorGraphSystem.h
@@ -7,12 +7,16 @@
 
 namespace Havtorn
 {
+	struct SSpriteAnimatorGraphComponent;
 	//SSpriteAnimatorGraphComponent
 	class CSpriteAnimatorGraphSystem : public ISystem
 	{
+	public:
 		CSpriteAnimatorGraphSystem();
 		~CSpriteAnimatorGraphSystem() override;
-
 		void Update(CScene* scene) override;
+
+	private:
+		SVector4 TickAnimationClip(SSpriteAnimatorGraphComponent& component, const F32 deltaTime);
 	};
 }
