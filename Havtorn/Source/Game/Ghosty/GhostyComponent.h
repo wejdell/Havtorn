721909d8a435959ad25f6658e78e6accc21c9fac
Axel Savage <axelsavage@gmail.com>
2023-08-09 16:30:09 +0200

    [Created] System / Component Ghosty

---
+++ Havtorn/Source/Game/Ghosty/GhostyComponent.h
@@ -0,0 +1,23 @@
+// Copyright 2022 Team Havtorn. All Rights Reserved.
+
+#pragma once
+#include "ECS/Component.h"
+
+namespace Havtorn
+{
+	struct SGhostyState
+	{
+		SVector Input;
+		bool IsInWalkingAnimationState;
+	};
+
+	struct SGhostyComponent : SComponent
+	{
+		SGhostyComponent() 
+			: SComponent(EComponentType::GhostyComponent)
+		{}
+		~SGhostyComponent() override { }
+
+		SGhostyState State;
+	};
+}
---
+++ Havtorn/Source/Game/Ghosty/GhostySystem.cpp
@@ -0,0 +1,79 @@
+// Copyright 2022 Team Havtorn. All Rights Reserved.
+
+#include "hvpch.h"
+#include "GhostySystem.h"
+#include "Scene/Scene.h"
+#include "Input/InputMapper.h"
+#include "GhostyComponent.h"
+
+namespace Havtorn
+{
+	CGhostySystem::CGhostySystem()
+		: ISystem()
+	{
+		GEngine::GetInput()->GetAxisDelegate(EInputAxisEvent::Right).AddMember(this, &CGhostySystem::HandleAxisInput);
+
+		EvaluateIdleFunc = std::bind(&CGhostySystem::EvaluateIdle, this, std::placeholders::_1, std::placeholders::_2);
+		EvaluateLocomotionFunc = std::bind(&CGhostySystem::EvaluateLocomotion, this, std::placeholders::_1, std::placeholders::_2);
+	}
+
+	CGhostySystem::~CGhostySystem()
+	{
+	}
+	void CGhostySystem::Update(CScene* scene)
+	{
+		F32 deltaTime = GTime::Dt();
+		auto& transformComponents = scene->GetTransformComponents();
+		auto& ghostyComponents = scene->GetGhostyComponents();
+		for (U32 i = 0; i < ghostyComponents.size(); i++)
+		{
+			SGhostyComponent& ghosty = ghostyComponents[i];
+			if (!ghosty.IsInUse)
+				continue;
+
+			ghosty.State.Input = input;
+
+			STransformComponent& transform = transformComponents[i];
+			if (ghosty.State.IsInWalkingAnimationState)
+			{
+				transform.Transform.Move(ghosty.State.Input * deltaTime);
+			}
+		}
+
+		ResetInput();
+	}
+
+	I16 CGhostySystem::EvaluateIdle(CScene* scene, U64 entitySceneIndex)
+	{
+		auto& ghostyComponent = scene->GetGhostyComponents()[entitySceneIndex];
+		if (ghostyComponent.State.Input.LengthSquared() > 0.0f)
+			return 1;
+
+		ghostyComponent.State.IsInWalkingAnimationState = false;
+		return 0;
+	}
+
+	I16 CGhostySystem::EvaluateLocomotion(CScene* scene, U64 entitySceneIndex)
+	{
+		auto& ghostyComponent = scene->GetGhostyComponents()[entitySceneIndex];
+		SVector stateInput = ghostyComponent.State.Input;
+
+		ghostyComponent.State.IsInWalkingAnimationState = true;
+
+		if (stateInput.X < 0)
+			return 0;
+
+		return 1;
+	}
+
+	void CGhostySystem::HandleAxisInput(const SInputAxisPayload payload)
+	{
+		input = SVector::Right * payload.AxisValue;
+	}
+
+	void CGhostySystem::ResetInput()
+	{
+		input.X = 0.0f;
+		input.Y = 0.0f;
+	}
+}
