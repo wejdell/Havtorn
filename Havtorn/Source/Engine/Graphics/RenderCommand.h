411885db9e58eff410733c6397558e5bd644a1c1
aki-gonzalez <gonzalez_aki@hotmail.com>
2023-08-26 17:50:22 +0200

    Documented data and comments

--- Havtorn/Source/Engine/Graphics/RenderCommand.h
+++ Havtorn/Source/Engine/Graphics/RenderCommand.h
@@ -126,6 +126,96 @@ namespace Havtorn
 		inline const SDebugShapeComponent& GetDebugShapeComponent() const { return DebugShapeComponent; }
 		inline const SMetaDataComponent& GetMetaDataComponent() const { return MetaDataComponent; }
 
+		/*
+		Will there (possibly) be more things that are not component-based that want to send RenderCommands?
+		yes - suggestions/currently
+		* DebugSystem
+
+		Do we want Havtorn to support RenderCommands that are not tied to Components?
+		(I say): yes. There can be times where users will want to send a custom RenderCommand that is not part of the ECS.
+		Also, everytime a new Component is created (currently) the component would have to be added to to the RenderCommand-struct.
+		Which might be tedious...? Having generalised data (even if not used with all commands) would open up some extra doors. 
+
+		Would be nice to decouple RenderCommands from Components. Just have data.
+		E.g data used by RenderManager exists in command.
+		*	SDrawCallData is used in several Render-methods in RenderManager
+		*	Instead of:
+		FrameBufferData.ToCameraFromWorld = spotLightComp.ShadowmapView.ShadowViewMatrix;
+		FrameBufferData.ToWorldFromCamera = spotLightComp.ShadowmapView.ShadowViewMatrix.FastInverse();
+		FrameBufferData.ToProjectionFromCamera = spotLightComp.ShadowmapView.ShadowProjectionMatrix;
+		FrameBufferData.ToCameraFromProjection = spotLightComp.ShadowmapView.ShadowProjectionMatrix.Inverse();
+		FrameBufferData.CameraPosition = spotLightComp.ShadowmapView.ShadowPosition;
+		It could be:
+		FrameBufferData.ToCameraFromWorld = command.ToCameraFromWorld;
+		FrameBufferData.ToWorldFromCamera = command.ToWorldFromCamera;
+		FrameBufferData.ToProjectionFromCamera = command.ToProjectionFromCamera;
+		FrameBufferData // ...
+
+		I.e: the data of a RenderCommand is already interpreted and the RenderManger only fetches the data it needs in the method
+		linked to by the type.
+
+		// [Currently] Decal specific
+		bool ShouldRenderAlbedo
+		bool ShouldRenderMaterial
+		bool ShouldRenderNormal
+
+		SMatrix ToCameraFromWorld
+		SMatrix ToWorldFromCamera
+		SMatrix ToProjectionFromCamera
+		SMatrix ToCameraFromProjection
+		SMatrix ToWorldFromObject
+
+		SMatrix ToWorld
+		SMatrix ToObjectSpace
+
+		SVector4 CameraPosition
+
+		string staticMeshName
+
+		F32 Thickness
+
+		SVector4 Direction
+		SVector4 DirectionNormal1
+		SVector4 DirectionNormal2
+		SColor Color
+		F32 Intensity
+		F32 Range
+		F32 OuterAngle
+		F32 InnerAngle
+
+		// Volumetric data
+		F32 NumberOfSmaplesReciprocal
+		F32 LightPower 
+		F32 ScatteringProbability
+		F32 HenyeyGreensteinGValue
+
+		U8 TopologyIndex
+		U8 PixelShaderIndex
+		U8 SamplerIndex
+		std::vector<U32> TextureBankIndices
+
+		SDrawCallData
+		U32 IndexCount
+		U16 VertexBufferIndex
+		U16 IndexBufferIndex
+		U16 VertexStrideIndex
+		U16 VertexOffsetIndex
+
+		std::vector<SShadowMapViewData>
+		SMatrix ShadowViewMatrix
+		SMatrix ShadowProjectionMatrix
+		SVector4 ShadowPosition
+		U16 ShadowmapViewportIndex
+
+		SVector2 ShadowmapResolution
+		SVector2 ShadowAtlasResolution
+		SVector2 ShadowmapStartingUV
+		F32	ShadowTestTolerance
+
+		F32 EmissiveStrength
+
+		*/
+
 		//SComponentArray Components;
 		ERenderCommandType Type;
 		STransformComponent TransformComponent;
