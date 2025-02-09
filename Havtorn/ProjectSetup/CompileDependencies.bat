ae7c59f16dee757c46640b8ba9717eeb8c54d758
Nicolas Risberg <nicolas.risberg@gmail.com>
2025-02-09 20:57:58 +0100

    Uncommented CompileDependencies.bat build lines

--- Havtorn/ProjectSetup/CompileDependencies.bat
+++ Havtorn/ProjectSetup/CompileDependencies.bat
@@ -2,26 +2,26 @@
 echo.
 
 cd ..\External\assimp
-REM echo Generating assimp CMake files...
-REM echo.
-REM cmake CMakeLists.txt
-REM echo.
-REM echo Building assimp...
-REM echo.
-REM cmake --build .
-REM echo.
+echo Generating assimp CMake files...
+echo.
+cmake CMakeLists.txt
+echo.
+echo Building assimp...
+echo.
+cmake --build .
+echo.
 copy bin\Debug\assimp-vc143-mtd.dll ..\..\Bin\
 copy lib\Debug\assimp-vc143-mtd.lib ..\Lib\Debug\
 cd .. 
 
 cd PhysX\physx
-REM echo Generating PhysX files...
-REM echo.
-REM call generate_projects.bat vc17win64 
-REM echo.
-REM echo Building PhysX...
-REM echo.
-REM cmake --build compiler/vc17win64 
+echo Generating PhysX files...
+echo.
+call generate_projects.bat vc17win64 
+echo.
+echo Building PhysX...
+echo.
+cmake --build compiler/vc17win64 
 copy bin\win.x86_64.vc143.md\debug\PhysX_64.dll ..\..\..\Bin\
 copy bin\win.x86_64.vc143.md\debug\PhysX_64.lib ..\..\Lib\Debug\PhysX\
 copy bin\win.x86_64.vc143.md\debug\PhysXCommon_64.dll ..\..\..\Bin\
@@ -35,33 +35,33 @@ copy bin\win.x86_64.vc143.md\debug\PhysXFoundation_64.lib ..\..\Lib\Debug\PhysX\
 copy bin\win.x86_64.vc143.md\debug\PhysXPvdSDK_static_64.lib ..\..\Lib\Debug\PhysX\
 copy bin\win.x86_64.vc143.md\debug\PhysXCharacterKinematic_static_64.lib ..\..\Lib\Debug\PhysX\
 copy bin\win.x86_64.vc143.md\debug\PhysXGpu_64.dll ..\..\..\Bin\
-REM echo.
+echo.
 cd ..\..
 
 cd box2d
-REM echo.
-REM rmdir /s /q build
-REM mkdir build
+echo.
+rmdir /s /q build
+mkdir build
 cd build
-REM echo Generating box2d files...
-REM cmake -DBOX2D_SAMPLES=OFF -DBOX2D_UNIT_TESTS=OFF -S ..
-REM echo.
-REM echo Building box2d...
-REM cmake --build .
-REM echo %cd%
+echo Generating box2d files...
+cmake -DBOX2D_SAMPLES=OFF -DBOX2D_UNIT_TESTS=OFF -S ..
+echo.
+echo Building box2d...
+cmake --build .
+echo %cd%
 copy src\Debug\box2dd.lib ..\..\Lib\Debug\
-REM echo.
+echo.
 cd ..
 cd ..
 
 cd DirectXTex
-REM echo Generating DirectXTex files...
-REM echo.
-REM cmake CMakeLists.txt -DBUILD_TOOLS=OFF -DBUILD_SHARED_LIBS=ON -DBUILD_SAMPLE=OFF
-REM echo.
-REM echo Building DirectXTex...
-REM echo.
-REM cmake --build . 
+echo Generating DirectXTex files...
+echo.
+cmake CMakeLists.txt -DBUILD_TOOLS=OFF -DBUILD_SHARED_LIBS=ON -DBUILD_SAMPLE=OFF
+echo.
+echo Building DirectXTex...
+echo.
+cmake --build . 
 copy bin\Debug\DirectXTex.dll ..\..\Bin\
 copy lib\Debug\DirectXTex.lib ..\Lib\Debug\
 cd .. 
