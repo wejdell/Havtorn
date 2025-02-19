6f3b32ed37e0fa2f43eb239608d35e5c45a910f5
Axel Savage <axelsavage@gmail.com>
2025-02-19 21:11:11 +0100

    [GUI] Added "EXPORT" macro to InitImgui + Deleted commented out stuff- from when GUIProcess held the Imgui Impl pointer

--- Havtorn/Source/GUI/GUIProcess.h
+++ Havtorn/Source/GUI/GUIProcess.h
@@ -20,7 +20,7 @@ namespace Havtorn
 		EXPORT ~GUIProcess() override;
 		bool Init() override;
 
-		void InitImGui(HWND hwnd, ID3D11Device* device, ID3D11DeviceContext* context);
+		EXPORT void InitImGui(HWND hwnd, ID3D11Device* device, ID3D11DeviceContext* context);
 
 		void BeginFrame() override;
 		void PreUpdate() override {}
@@ -30,21 +30,7 @@ namespace Havtorn
 
 		void WindowsWindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
 
-		//ImGui Wrap
-		//static void BeginGUI(const char* aName, bool* open = 0, int flags = 0);
-		//static void EndGUI();
-		//static void Text(const char* fmt, ...);
-		//static bool DragFloat(const char* label, float* value, float min, float max, float speed = 0.1f);
-		//static bool DragFloat2(const char* label, float* value, float min, float max, float speed = 0.1f);
-		//static bool DragFloat3(const char* label, float* value, float min, float max, float speed = 0.1f);
-		//static bool DragFloat4(const char* label, float* value, float min, float max, float speed = 0.1f);
-		//static bool CheckBox(const char* label, bool* v);
-
 	private:
 		std::unique_ptr<GUI> EditorGUI;
-		//class _declspec(dllexport) GUIPimpl;
-		//GUIPimpl* Impl;
-
-		//static GUIProcess* Instance;
 	};
 }
