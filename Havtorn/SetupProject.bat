dff465cd4a6cdc74ac7de8fe98381283871fa5b2
Aki <gonzalez_aki@hotmail.com>
2025-07-20 16:01:59 +0200

    wip python installation

--- Havtorn/SetupProject.bat
+++ Havtorn/SetupProject.bat
@@ -20,24 +20,7 @@ goto :NO_INSTALL_PYTHON
 
 :INSTALL_PYTHON
 echo.
-echo:Downloading Python
 if not exist "SetupRequirements//" mkdir SetupRequirements
-rem # get latest download url
-rem $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-rem $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
-rem        Select-Object -ExpandProperty "assets" |
-rem        Where-Object "browser_download_url" -Match '.msixbundle' |
-rem        Select-Object -ExpandProperty "browser_download_url"
-
-rem # download
-rem Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
-
-rem # install
-rem Add-AppxPackage -Path "Setup.msix"
-
-rem # delete file
-rem Remove-Item "Setup.msix"
-
 set "pythonVersion=3.13.5" 
 set "pythonDownloadUrl=https://www.python.org/ftp/python/%pythonVersion%/python-%pythonVersion%-amd64.exe" 
   
@@ -47,10 +30,11 @@ set "installDir=SetupRequirements"
 
 rem Download Python installer 
 rem bitsadmin.exe /transfer "PythonInstaller" "%pythonDownloadUrl%" "SetupRequirements\python-%pythonVersion%-amd64.exe" 
-echo: Downloading to "%~dp0%downloadDir%\python-%pythonVersion%-amd64.exe"
-bitsadmin /transfer pythonDownload /download /priority high "%pythonDownloadUrl%" "%~dp0\\%downloadDir%\\python-%pythonVersion%-amd64.exe"
-rem Install Python silently 
-rem"%TEMP%\python-installer.exe" /quiet InstallAllUsers=1 PrependPath=1 DefaultCustomInstall=1 DefaultPath=%installDir% /wait 
+echo:Downloading to "%~dp0%downloadDir%\installer-python-%pythonVersion%-amd64.exe"
+PAUSE
+bitsadmin /transfer pythonDownload /download /priority high "%pythonDownloadUrl%" "%~dp0%downloadDir%\installer-python-%pythonVersion%-amd64.exe"
+if not exist "SetupRequirements//Python//" mkdir SetupRequirements/Python
+"%~dp0\\%downloadDir%\\installer-python-%pythonVersion%-amd64.exe" InstallAllUsers=1 AssociateFiles=1 PrependPath=1 AppendPath=0 DefaultPath=%installDir% /wait 
   
 rem Clean up 
 rem del "%TEMP%\python-installer.exe" /f /q 
