9cc9cfe4a458fe9c7e04fc7888ccc4f49305a1af
Aki <gonzalez_aki@hotmail.com>
2025-07-29 14:10:55 +0200

    cleanup

--- Havtorn/TestCmakeInstall.bat
+++ Havtorn/TestCmakeInstall.bat
@@ -2,6 +2,7 @@
 :: Check for CMake installation
 :: Check through PATH
 @echo off
+set cmakeVersion=3.31.4
 @ cmake --version 3>NUL
 if %errorlevel% == 0 goto :CMAKE_INSTALLED
 goto :CMAKE_NOT_INSTALLED
@@ -9,15 +10,19 @@ goto :CMAKE_NOT_INSTALLED
 :CMAKE_NOT_INSTALLED
 :: check if /req-dir/ exists -> yes, continue; no goto CMAKE_INSTALL
 :: check if /req-dir/CMake/?/?/cmake.exe --version -> yes, goto CMAKE_SET_VARIABLE; no goto CMAKE_INSTALL
-if not exist "SetupRequirements//" goto :CMAKE_INSTALL_PERMISSION
-if not exist "SetupRequirements/CMake//" goto :CMAKE_INSTALL_PERMISSION
+if not exist "SetupRequirements/CMake/cmake-%cmakeVersion%-windows-x86_64/bin//" goto :CMAKE_INSTALL_PERMISSION
+echo Local CMake directory found
+echo %~dp0SetupRequirements\CMake\cmake-%cmakeVersion%-windows-x86_64\bin\cmake.exe
+:: cmake-3.31.4-windows-x86_64\bin
 :: Can't validate this check currently, without install: run CMake.exe from a custom path
+%~dp0SetupRequirements/CMake/cmake-%cmakeVersion%-windows-x86_64/bin/cmake.exe --version 3>NUL
+if %errorlevel% == 0 goto :CMAKE_SET_VARIABLE
 goto :CMAKE_INSTALL_PERMISSION
 
 :CMAKE_INSTALLED
 echo CMake is installed, found through PATH
 PAUSE
-goto :eof
+goto :RUN_NEXT
 
 :CMAKE_INSTALL_PERMISSION
 :: ask permission -> yes, continue; no, goto REQUIREMENT_ERROR_OUT
@@ -30,24 +35,15 @@ goto :REQUIREMENT_ERROR_OUT
 :CMAKE_INSTALL
 if not exist "SetupRequirements//" mkdir SetupRequirements
 if not exist "SetupRequirements//CMake//" mkdir SetupRequirements\CMake
-set cmakeVersion=3.31.4
 set downloadDir=SetupRequirements//
 set extractedDirectory=SetupRequirements//CMake//
 set cmakeZipUrls=https://github.com/Kitware/CMake/releases/download/v%cmakeVersion%/cmake-%cmakeVersion%-windows-x86_64.zip
 
-echo %cmakeVersion%
-echo %downloadDir%
-echo %extractedDirectory%
-echo %cmakeZipUrls%
-echo "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip"
-echo "%~dp0%extractedDirectory%"
-PAUSE
 :: Download Python installer 
-::echo:Downloading to "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip"
-::PAUSE
-::bitsadmin /transfer cmakeDownload /download /priority high "%cmakeZipUrls%" "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip"
-::PAUSE
-rem Could do silent/quiet install? e.g do not show installer window
+echo:Downloading to "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip" and then extracting to "%~dp0%extractedDirectory%"
+PAUSE
+bitsadmin /transfer cmakeDownload /download /priority high "%cmakeZipUrls%" "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip"
+
 :: x - extract, v - verbose, f - target archive, C - extraction directory
 tar -xvf "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip" -C "%~dp0%extractedDirectory%"
 PAUSE
@@ -58,9 +54,17 @@ PAUSE
 
 :CMAKE_SET_VARIABLE
 :: set cmake=/req-dir/CMake/cmake.exe
-goto :eof
+echo Setting cmake variable
+set cmake=%~dp0SetupRequirements/CMake/cmake-%cmakeVersion%-windows-x86_64/bin/cmake.exe
+PAUSE
+goto :RUN_NEXT
 
 :REQUIREMENT_ERROR_OUT
 :: echo Failed to install setup requirement, please allow install or install requirement manually
 echo Error^: Failed to find or install requirement, either allow setup to install or do a manual installation.
 PAUSE
+
+:RUN_NEXT
+echo SetupRequirements complete, running next script
+echo.
+%0
