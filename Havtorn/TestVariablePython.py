1b1def377a962d91dfa3110ef3b155802f2bdfe1
Aki <gonzalez_aki@hotmail.com>
2025-07-31 19:18:57 +0200

    added clarification

--- Havtorn/TestVariablePython.py
+++ Havtorn/TestVariablePython.py
@@ -6,5 +6,6 @@ import pprint
 
 print("starting subprocess TestVariableSubScript.bat from TestVariablePython.py")
 subprocess.call([os.path.abspath("./TestVariableSubScript.bat"), "nopause"])
+print("should fail, variable was set in subprocess which is not permitted")
 print(f"testVariable is {os.environ["testVariable"]}, set in subprocess read from from TestVariablePython.py")
 input()
