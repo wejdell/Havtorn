00bd5038fae1205d3921a66b907f1c85b105a7b9
Aki <gonzalez_aki@hotmail.com>
2025-08-03 20:22:55 +0200

    cmake generation tests

--- Havtorn/GenerateProject.bat
+++ Havtorn/GenerateProject.bat
@@ -2,6 +2,6 @@
 echo.
 echo Generating CMake files...
 echo.
-cmake -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
+cmake -D CMAKE_GENERATOR_INSTANCE:location="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
 echo.
 PAUSE
--- Havtorn/ProjectSetup/GenerateProjectFiles.bat
+++ Havtorn/ProjectSetup/GenerateProjectFiles.bat
@@ -1,14 +1,3 @@
 @echo off
 cmake -G "Visual Studio 17 2022" -A x64 -S ../Source -B ../BuildFiles
 PAUSE
-REM @echo.
-REM @echo Checking license in files...
-
-REM set fileExtentions=.cpp .h .hlsl .hlsli
-REM set command=call %~dp0CheckFileForLicense.bat @path
-
-REM for %%G in (%fileExtentions%) do (
-REM	forfiles /p Source /s /m *%%G /C "cmd /c %command%"
-REM )
-
-@echo.
