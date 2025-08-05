06c3081528314db2fc81f3c2837f4043ad43c841
Aki <gonzalez_aki@hotmail.com>
2025-08-05 17:07:10 +0200

    test call & redirect packman install dir

--- Havtorn/ProjectSetup/CompileDependencies.bat
+++ Havtorn/ProjectSetup/CompileDependencies.bat
@@ -1,28 +1,35 @@
 @echo off
-
+:: move these down to before physx
 mkdir -p ..\External\Lib\Debug\PhysX\
 mkdir -p ..\External\Lib\Release\PhysX\
 
-cd ..\External\assimp
-echo.
-echo Generating assimp CMake files...
-echo.
-cmake -G "Visual Studio 17 2022" CMakeLists.txt
-rem cmake -G "Visual Stduio Build Tools 2022" CMakeLists.txt
-echo.
-echo Building assimp...
-echo.
-cmake --build .
-echo.
-copy bin\Debug\assimp-vc143-mtd.dll ..\..\Bin\
-copy lib\Debug\assimp-vc143-mtd.lib ..\Lib\Debug\
-PAUSE
-cd .. 
+::cd ..\External\assimp
+::echo.
+::echo Generating assimp CMake files...
+::echo.
+::cmake -G "Visual Studio 17 2022" CMakeLists.txt
+::rem cmake -G "Visual Stduio Build Tools 2022" CMakeLists.txt
+::echo.
+::echo Building assimp...
+::echo.
+::cmake --build .
+::echo.
+::copy bin\Debug\assimp-vc143-mtd.dll ..\..\Bin\
+::copy lib\Debug\assimp-vc143-mtd.lib ..\Lib\Debug\
+::PAUSE
+::cd .. 
+
+::remove this line after testing packman redirect
+cd ..\External\
 
 cd PhysX\physx
 echo.
 echo Generating PhysX files...
 echo.
+:: packman init script will only read this path correctly if it is without ""
+set PM_PACKAGES_ROOT=%~dp0/../SetupRequirements/packman
+if not exist "%PM_PACKAGES_ROOT%/" mkdir "%PM_PACKAGES_ROOT%"
+PAUSE
 call generate_projects.bat vc17win64 
 echo.
 echo Building PhysX...
