6706f4354081aceb86abb024d028a484f942f1b1
Aki <gonzalez_aki@hotmail.com>
2025-08-05 17:14:23 +0200

    batch method call test

--- Havtorn/TestCallTestVariableSubScript.bat
+++ Havtorn/TestCallTestVariableSubScript.bat
@@ -1,4 +1,22 @@
 @echo off
+
+
+echo calling TestVariableSubScript.bat
 call TestVariableSubScript.bat
-echo testVariable is %testVariable%
+echo.
+echo reading testVariable after calling .bat is %testVariable%
 PAUSE
+
+echo.
+echo calling MethodTest
+call :MethodTest param1 what
+echo.
+echo after MethodTest: methodVariable is %methodVariable%
+PAUSE
+
+:MethodTest
+echo this is a method test
+echo setting a methodVariable
+echo 2 params are %~1 and %~2 
+set methodVariable="Method says hello"
+goto:eof
