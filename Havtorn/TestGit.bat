c72056c323f6e4f49453bdf3ad80c6bdd2e747a9
Aki <gonzalez_aki@hotmail.com>
2025-08-07 16:28:46 +0200

    GitAdditionalSetup.bat

--- Havtorn/TestGit.bat
+++ Havtorn/TestGit.bat
@@ -1,72 +1,9 @@
 @echo off
-echo Git Submodule Requirement Step 
-echo Looking for git cli availability
-:: -v = version, >NUL 2>&1 = hides output of command
-git -v >NUL 2>&1
-if %errorlevel% NEQ 0 goto :GIT_CLI_NOT_AVAILABLE
-set git=git
-goto :GIT_CLI_AVAILABLE
-
-goto :GIT_CLI_NOT_AVAILABLE
-
-:GIT_SET_LOCALEXE
-::echo %PATH%
-set PATH=%~dp0SetupRequirements\PortableGit\bin\;%PATH%
-::echo.
-::echo %PATH%
-::setx git %~dp0SetupRequirements\PortableGit\bin\git.exe
-::setx PATH "%PATH%;%~dp0SetupRequirements\PortableGit\bin\"
-::echo setx used to set git: 
-::set setxUsed=true
-goto :GIT_CLI_AVAILABLE
-
-:GIT_CLI_AVAILABLE
-echo git command available
+call ProjectSetup/GitAdditionalSetup.bat
 echo.
-echo updating submodules
-git submodule update --init --recursive
-echo.
-echo enabling lfs
-git lfs install
-echo.
-echo configure git longpaths 
-git config core.longpaths true
-echo.
+echo Git Additional Setup complete with error level: %errorlevel%
 PAUSE
-goto :eof
-
-:GIT_CLI_NOT_AVAILABLE
 echo.
-echo Git for the command line not found 
-echo Searching for local portable git...
-if not exist "SetupRequirements/PortableGit/bin//" goto :GIT_INSTALL_PERMISSION
-echo Searching for executable...
-SetupRequirements\PortableGit\bin\git.exe -v >NUL 2>&1
-if %errorlevel% NEQ 0 goto :GIT_INSTALL_PERMISSION
-goto :GIT_SET_LOCALEXE
-
-:GIT_INSTALL_PERMISSION
-echo.
-echo Permission to download git for the command line?
-choice /C yn /M "Yes/No?"
-if %errorlevel% == 1 goto :GIT_DOWNLOAD
-goto :REQUIREMENT_ERROR_OUT
-
-:GIT_DOWNLOAD
-if not exist "SetupRequirements//" mkdir SetupRequirements
-set downloadDir=SetupRequirements//
-set extractedDirectory=SetupRequirements//
-set gitPortableUrl=https://github.com/git-for-windows/git/releases/download/v2.50.1.windows.1/PortableGit-2.50.1-64-bit.7z.exe
-
-::echo Downloading to "%~dp0%downloadDir%\"...
-::PAUSE
-::bitsadmin /transfer gitDownload /download /priority high "%gitPortableUrl%" "%~dp0%downloadDir%\PortableGit-2.50.1-64-bit.7z.exe"
-
-echo Extracting to "%~dp0%extractedDirectory%\PortableGit" ...
-SetupRequirements\PortableGit-2.50.1-64-bit.7z.exe -y
-:: x - extract, v - verbose, f - target archive, C - extraction directory
-::tar -xvf "%~dp0%downloadDir%\cmake-%cmakeVersion%-windows-x86_64.zip" -C "%~dp0%extractedDirectory%"
-echo Git portable download complete, continuing with setup
-:: del %~dp0%downloadDir%\PortableGit-2.50.1-64-bit.7z.exe
+echo Checking to see if git command works
+git -v
 PAUSE
-goto :GIT_SET_LOCALEXE
--- Havtorn/TestGitVar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-::for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set my_user_path=%%B
-::echo %my_user_path%
-::setx PATH "%~dp0SetupRequirements\PortableGit\bin\;%my_user_path%"
-::set PATH="%~dp0SetupRequirements\PortableGit\bin\;%my_user_path%"
-:: Modify PATH only for the lifetime of the current process
-set PATH=%~dp0SetupRequirements\PortableGit\bin\;%PATH%
-git -v
-PAUSE
