f9f84163984da867c8f7136fc350fa7b4ff0dd28
Aki <gonzalez_aki@hotmail.com>
2025-08-07 14:38:17 +0200

    CMake verification and installation works

--- Havtorn/TestRunCMakeInstall.bat
+++ Havtorn/TestRunCMakeInstall.bat
@@ -1,7 +1,10 @@
 @echo off
-call TestCmakeInstall.bat
+call ProjectSetup/CMakeSetup.bat
 echo.
 echo CMake Install complete with error level: %errorlevel%
 PAUSE
+echo.
+echo Checking to see if cmake command works
 cmake --version
 PAUSE
+call GenerateProject.bat
