a19a1bc0e5dddcb8a8d8e2f8784415045bb4ad00
Axel Savage <axelsavage@gmail.com>
2025-08-10 14:08:58 +0200

    Removed Component's SComponentView
    Not neccesary to have this here - But it's at least in the history for reference :pray:

--- Havtorn/Source/Engine/ECS/Component.h
+++ Havtorn/Source/Engine/ECS/Component.h
@@ -7,39 +7,6 @@ namespace Havtorn
 {
 	class CScene;
 
-
-
-	template<typename TValidate>
-	concept IsValidatable = requires(const TValidate & toValidate)
-	{
-		{ toValidate.Validate() } -> std::convertible_to<bool>;
-	};
-
-	struct SComponentView
-	{
-	public:
-
-		//TValidate must be a type with a method with the name and signature -> bool Valiate() const
-		template<IsValidatable TValidate>
-		explicit SComponentView(const TValidate& toValidate)
-			: Component(&toValidate)
-			, ValidateImpl	{ 
-								[](const void *obj)
-								{
-									return static_cast<const TValidate*>(obj)->Validate();
-								}
-							}
-		{ }
-
-		bool Validate() const { return ValidateImpl(Component); }
-
-	private:
-		const void* Component;
-		bool (*ValidateImpl)(const void*);
-
-	};
-
-
 	struct ENGINE_API SComponent
 	{
 		SComponent() = default;
