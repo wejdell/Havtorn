d0088b2c4326e750b4e6ccac0024309769cc9209
Aki <gonzalez_aki@hotmail.com>
2025-08-13 16:53:34 +0200

    Main setup scripts updated

--- Havtorn/GenerateProject.bat
+++ Havtorn/GenerateProject.bat
@@ -1,8 +1,5 @@
 @echo off
-echo.
-echo Generating CMake files...
-echo.
-cmake -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
+cd %~dp0\ProjectSetup\
+GenerateProjectFiles.bat
+::cmake -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
 ::cmake -D CMAKE_GENERATOR_INSTANCE:location="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
-echo.
-PAUSE
--- Havtorn/ProjectSetup/GenerateProjectFiles.bat
+++ Havtorn/ProjectSetup/GenerateProjectFiles.bat
@@ -1,3 +1,4 @@
 @echo off
+echo Generating CMake files...
 cmake -G "Visual Studio 17 2022" -A x64 -S ../Source -B ../BuildFiles
 PAUSE
