430daf9edeec63e753efa63aad9cd369a6ec8455
Aki <gonzalez_aki@hotmail.com>
2025-08-14 11:18:32 +0200

    python module testing

--- Havtorn/ProjectSetup/MasterSetup.py
+++ Havtorn/ProjectSetup/MasterSetup.py
@@ -5,8 +5,10 @@ import platform
 from SetupMSVC import MSVCConfiguration as MSVCRequirements
 MSVCRequirements.Validate()
 
+print("Python modules")
 from SetupPython import PythonConfiguration as PythonRequirements
 PythonRequirements.Validate()
+print("Python moduels complete")
 
 subprocess.call([os.path.abspath("./GenerateProjectFiles.bat"), "nopause"])
 subprocess.call([os.path.abspath("./CompileDependencies.bat"), "nopause"])
