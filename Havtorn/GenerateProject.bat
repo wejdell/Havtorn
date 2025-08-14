ecaca8d142424d10c62a330de0da01883ef90486
Aki <gonzalez_aki@hotmail.com>
2025-08-14 17:10:42 +0200

    Flowchart & tweaks

--- Havtorn/GenerateProject.bat
+++ Havtorn/GenerateProject.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0\ProjectSetup\
-CompileDependencies.bat
 GenerateProjectFiles.bat
+CompileDependencies.bat
 ::cmake -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
 ::cmake -D CMAKE_GENERATOR_INSTANCE:location="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" -G "Visual Studio 17 2022" -A x64 -S Source -B BuildFiles
