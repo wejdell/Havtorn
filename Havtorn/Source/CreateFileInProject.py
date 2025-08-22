07799bc38f32c077cd809d09a24deaab640396fc
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-08-22 14:56:31 +0200

    Fix undo

--- Havtorn/Source/CreateFileInProject.py
+++ Havtorn/Source/CreateFileInProject.py
@@ -1,24 +1,25 @@
 import os
 import subprocess
+import re
 
 from ValidationUtils import ValidationUtil
 
-# TODO: Look over if it is possible to restructure how CMakeLists and this script tracks directories
+# TODO: Look over if it is possible to restructure how CMakeLists and this script tracks directories -> Generate CMakeLists through script?
 havtornLicense="// Copyright 2025 Team Havtorn. All Rights Reserved."
 cmakeListFilePath="CMakeLists.txt"
 
-core="core"
-platform="platform"
-gui="gui"
-imgui="imgui"
-engine="engine"
-shaderinclude="shaderinclude"
-vertex="vertexshader"
-geometry="geometryshader"
-pixel="pixelshader"
-game="game"
-editor="editor"
-launcher="launcher"
+core = "core"
+platform = "platform"
+gui = "gui"
+imgui = "imgui"
+engine = "engine"
+shaderinclude = "shaderinclude"
+vertex = "vertexshader"
+geometry = "geometryshader"
+pixel = "pixelshader"
+game = "game"
+editor = "editor"
+launcher = "launcher"
 
 folderChoices={
     core,
@@ -72,10 +73,9 @@ print("Pick a main directory:")
 for choice in folderChoices:
     print("\t" + choice)
 
-print("Directory:")
 chosenMainFolder=""
 while(True):
-    chosenMainFolder=input()
+    chosenMainFolder=input(">> Choose directory:")
     if chosenMainFolder in folderChoices:
         break
     else:
@@ -92,19 +92,20 @@ print(f' {undoFileCommand} to undo file, example: {undoFileCommand} 1')
 print(f' {undoDirectoryCommand} to undo last entered directory, example: {undoDirectoryCommand}')
 print(f' {continueCommand} to continue with creating files')
 
-additionalDirectories = ""
+subDirectories = ""
 filesToAdd = []
 while(True):
-    # Empty line
-    print()
+    print()# Empty line, for readability
+    print(f"Full directory: {choiceToPath[chosenMainFolder]}{subDirectories}")
+    print(f"Files:")
     for file in filesToAdd:
-        print(f'+ [{filesToAdd.index(file)}] {choiceToPath[chosenMainFolder]}{additionalDirectories}{file}')
+        print(f'+ [{filesToAdd.index(file) + 1}] {choiceToPath[chosenMainFolder]}{subDirectories}{file}')
     
-    addInput=input()
+    addInput=input(">> ")
     if continueCommand in addInput:
         break
 
-    if fileCommand in addInput:        
+    if fileCommand in addInput: 
         fileToAdd = "".join(addInput.replace(f"{fileCommand}", '').split())
         if fileToAdd == "":
             continue
@@ -113,14 +114,13 @@ while(True):
         if ValidationUtil.validate_file_name(filenameSplitForValidation[0]) is False:
             print("Error: filename contains invalid characters")
             continue
-        if (len(filenameSplitForValidation) == 1 is True # Missing extension
-            or len(filenameSplitForValidation) > 2 is True # More than 1 extension
+        if (len(filenameSplitForValidation) == 1 # Missing extension
+            or len(filenameSplitForValidation) > 2 # More than 1 extension
             or ValidationUtil.validate_file_extension(filenameSplitForValidation[1]) is False):
             print("Error: invalid extension")
             continue
         
         filesToAdd.append(fileToAdd)
-        print(f"> Added file: {fileToAdd}")
         continue
     
     if directoryCommand in addInput:
@@ -132,43 +132,59 @@ while(True):
             print("Error: directory name contains invalid characters")
             continue
 
-        additionalDirectories += directoryToAdd + "/"
-        print(f"> Added directory: {additionalDirectories}")
+        subDirectories += directoryToAdd + "/"
         continue
     
+    # TODO: figure out how handle multiple indices at once
     if undoFileCommand in addInput:
-        fileToUndo = "".join(addInput.replace(f"{undoFileCommand}", '').split())
+        filesToUndo = re.findall('[0-9]+', addInput)
+        if len(filesToUndo) == 0:
+            continue
+        undoFileIndex = int(filesToUndo[0]) - 1 # To the user we display them as index+1, meaning input 1 == index 0
+        if (len(filesToAdd) == 0
+            or undoFileIndex >= len(filesToAdd)):
+            continue
+
+        del filesToAdd[undoFileIndex]
         continue
     
     if undoDirectoryCommand in addInput:
+        try:
+            subDirectories = subDirectories.removesuffix('/')
+            subDirectories = subDirectories[:subDirectories.rindex('/')] + "/"
+        except:
+            subDirectories = ""
         continue
 
 # Empty line, for readability
 print()
 
-if not os.path.exists(choiceToPath[chosenMainFolder] + additionalDirectories):
-    os.makedirs(choiceToPath[chosenMainFolder] + additionalDirectories)
+if not os.path.exists(choiceToPath[chosenMainFolder] + subDirectories):
+    os.makedirs(choiceToPath[chosenMainFolder] + subDirectories)
 
+# create the files
 for fileName in filesToAdd:
     try:
-        with open(choiceToPath[chosenMainFolder] + additionalDirectories + fileName, "x") as file:
+        with open(choiceToPath[chosenMainFolder] + subDirectories + fileName, "x") as file:
             file.write(havtornLicense)
             print(f'> File "{file}" created')
     except FileExistsError:
-        print(f'! "{additionalDirectories + fileName}" already exists')
+        print(f'! "{choiceToPath[chosenMainFolder] +subDirectories + fileName}" already exists')
 
-# TODO: add to CMakeList.txt
+# Read CMakeLists into a list of lines, append new entires and rewrite file
 target=f"set({choiceToCollection[chosenMainFolder]}\n"
-
 for fileToAdd in filesToAdd:
-    entry=f"\t${choiceToPath[chosenMainFolder]}{additionalDirectories}{fileToAdd}\n"
+    entry=f"\t${choiceToPath[chosenMainFolder]}{subDirectories}{fileToAdd}\n"
     fileAsLineList=list[str]
     with open(cmakeListFilePath, "r") as cmakeFile: 
         fileAsLineList = cmakeFile.readlines()
-        # print(fileAsLineList)
+        # for l in fileAsLineList:
+        #     print(l)
+        # input()
         fileAsLineList.insert(fileAsLineList.index(target) + 1, entry)
         cmakeFile.flush()
     with open(cmakeListFilePath, "w") as cmakeFile:
         cmakeFile.writelines(fileAsLineList)
 
+print("\nRegenerating project ...")
 subprocess.call([os.path.abspath("./../ProjectSetup/GenerateProjectFiles.bat"), "nopause"])
