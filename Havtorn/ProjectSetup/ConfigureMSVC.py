80951d9f909fd957665b63602fb456afd4a127ae
Axel Savage <axelsavage@gmail.com>
2025-09-04 17:32:10 +0200

    You can now run urls with "Havtorn::/any string or id or whatever here" and it will run through havtorn Engine

--- Havtorn/ProjectSetup/ConfigureMSVC.py
+++ Havtorn/ProjectSetup/ConfigureMSVC.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import winreg
 from pathlib import Path
 
 class MSVCConfiguration:
@@ -8,6 +9,7 @@ class MSVCConfiguration:
 
     @classmethod
     def Validate(cls):
+        cls.InstallDeeplink()
         if (not cls.CheckIfMSVCInstalled()):
             print("MSVC is not installed.")
             return False
@@ -36,6 +38,24 @@ class MSVCConfiguration:
         subprocess.check_call([os.path.abspath("./InstallBuildTools.bat"), "nopause"])
 
         return True
+    
+    @classmethod
+    def InstallDeeplink(cls):
+        print("Adding Deeplink to Registry")
+
+        PROTOCOL_NAME = "Havtorn"
+        ENGINE_PROTOCOL = fr"URL:{PROTOCOL_NAME}"
+        LAUNCHER_PATH = str(os.path.abspath("../Bin/Launcher.exe"))
+
+        base = winreg.CreateKey(winreg.HKEY_CURRENT_USER, fr"Software\Classes\{PROTOCOL_NAME}")
+        winreg.SetValueEx(base, None, 0, winreg.REG_SZ, ENGINE_PROTOCOL)
+        winreg.SetValueEx(base, "URL Protocol", 0, winreg.REG_SZ, "")
+
+        icon = winreg.CreateKey(base, "DefaultIcon")
+        winreg.SetValueEx(icon, None, 0, winreg.REG_SZ, f"\"{LAUNCHER_PATH}\",1")
+
+        cmd = winreg.CreateKey(base, r"shell\open\command")
+        winreg.SetValueEx(cmd, None, 0, winreg.REG_SZ, f"\"{LAUNCHER_PATH}\" \"%1\"")
 
 if __name__ == "__main__":
     MSVCConfiguration.Validate()
