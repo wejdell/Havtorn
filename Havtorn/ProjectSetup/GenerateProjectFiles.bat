ee9e242303b1242107139ffbf3f6bbe4455d842d
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-09-05 13:43:59 +0200

    switch back to not specifying g path

--- Havtorn/ProjectSetup/GenerateProjectFiles.bat
+++ Havtorn/ProjectSetup/GenerateProjectFiles.bat
@@ -2,5 +2,6 @@
 call %~dp0\SetupCMake.bat
 if %errorlevel% NEQ 0 EXIT /B 1
 echo Generating CMake files...
+::cmake -D CMAKE_GENERATOR_INSTANCE:location="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" -G "Visual Studio 17 2022" -A x64 -S ../Source -B ../BuildFiles
 cmake -G "Visual Studio 17 2022" -A x64 -S ../Source -B ../BuildFiles
 PAUSE
