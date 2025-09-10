22d69d0818ec502d71e522200c8e855b38789ec4
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-09-10 15:20:18 +0200

    Additions to fix errors when testing

--- Havtorn/BuildProject.bat
+++ Havtorn/BuildProject.bat
@@ -1,6 +1,8 @@
 @echo off
 call %~dp0\ProjectSetup\SetupCMake.bat
 if %errorlevel% NEQ 0 EXIT /B 1
+call %~dp0\ProjectSetup\SetupBuildTools.bat
+if %errorlevel% NEQ 0 EXIT /B 1
 echo.
 echo Generating CMake files...
 echo.
--- Havtorn/ProjectSetup/InstallBuildTools.bat
+++ Havtorn/ProjectSetup/InstallBuildTools.bat
@@ -1,3 +1,3 @@
 @echo off
 ::TODO: figure out how to handle the update scenario. To update: vs_BuildTools.exe update --passive --wait --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended
-vs_BuildTools.exe --passive --wait --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended
+%~dp0vs_BuildTools.exe --passive --wait --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended
