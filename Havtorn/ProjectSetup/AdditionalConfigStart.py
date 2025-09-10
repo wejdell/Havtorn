e6428aeddba75a74b40fbbf4a250a93c349bc2df
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-09-10 14:46:28 +0200

    Configure -> AdditionalConfig rename

--- Havtorn/ProjectSetup/AdditionalConfigStart.py
+++ Havtorn/ProjectSetup/AdditionalConfigStart.py
@@ -1,11 +1,7 @@
 import os
 import subprocess
-import platform
 
-from ConfigureMSVC import MSVCConfiguration as MSVCRequirements
-MSVCRequirements.Validate()
-
-from ConfigurePython import PythonConfiguration as PythonRequirements
+from AdditionalConfigPython import PythonConfiguration as PythonRequirements
 PythonRequirements.Validate()
 
 subprocess.call([os.path.abspath("./GenerateProjectFiles.bat"), "nopause"])
