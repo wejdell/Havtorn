68531674b0ca2f1d3dc05f00a4c53a4153479fd7
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-09-19 21:18:44 +0200

    cleanup

--- Havtorn/Source/CreateFileInProject.py
+++ Havtorn/Source/CreateFileInProject.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import re
 import io
+import time
 
 from ValidationUtils import ValidationUtil
 
@@ -11,9 +12,13 @@ class FileCreationUtil:
     # class variables, should not be altered
     # TODO: If more filetypes are supported the characters used for the license comment need to be filtered, same goes for namespace structure
     havtornLicense="// Copyright 2025 Team Havtorn. All Rights Reserved.\n\n"
-    havtornNameSpace="\nnamespace Havtorn\n{\n\n}\n" # TODO: true for all file-types?
+    havtornNameSpace="\nnamespace Havtorn\n{\n\n}\n"
     cmakeListFilePath="CMakeLists.txt"
     
+    inputCharacters = ">> "
+
+    generatorScriptPath = "./../ProjectSetup/GenerateProjectFiles.bat"
+
     core = "core"
     platform = "platform"
     gui = "gui"
@@ -46,7 +51,7 @@ class FileCreationUtil:
         launcher
     }
     # So we always show them in the same order
-    mainFolderChoices=sorted(mainFolderChoices) # does this work?
+    mainFolderChoices=sorted(mainFolderChoices)
 
     choiceToCollection={
         core:"CORE_FILES",
@@ -83,28 +88,10 @@ class FileCreationUtil:
         launcher:"Launcher/",
     }
 
-    # TODO: would be nice not to need this, if using a script to generate CMakeLists this could perhaps be avoided
-    choiceToCMakeFolderVar={
-        core:"CORE_FOLDER",
-        platform:"PLATFORM_FOLDER",
-        gui:"GUI_FOLDER",
-        imgui:"IMGUI_FOLDER",
-        imguizmo:"IMGUIZMO_FOLDER",
-        imguinode:"IMGUI_NODE_FOLDER",
-        engine:"ENGINE_FOLDER",
-        shaderinclude:"SHADER_INCL_FOLDER",
-        vertex:"SHADER_FOLDER",
-        geometry:"SHADER_FOLDER",
-        pixel:"SHADER_FOLDER",
-        game:"GAME_FOLDER",
-        editor:"EDITOR_FOLDER",
-        launcher:"LAUNCHER_FOLDER",
-    }
-
     addFileCommand = "-f"
-    addFileNoExtraCommand = "-sf"
+    addFileSingle = "-sf"
     undoFileCommand = "-u"
-    switchMainDirCommand = "-m"
+    switchMainCommand = "-m"
     continueCommand = "-c"
 
     @classmethod
@@ -116,61 +103,55 @@ class FileCreationUtil:
     @classmethod
     def print_command_separator(self):
         print()
+        return
 
     @classmethod
-    def on_error(self, errorMessage):
+    def on_error(self, errorMessage:str):
         print(f'<!> {errorMessage}')
-        input()
+        time.sleep(1)
+        return
 
     @classmethod
-    def select_main_dir(self):
+    def select_main_folder(self):
         self.print_command_separator()
-        print("Pick a main directory:")
+        print("Pick a main folder:")
         for choice in self.mainFolderChoices:
             print("\t" + choice)
 
         self.print_command_separator()
         while(True):
-            self.mainFolder=input(">>:")
+            self.mainFolder=input(self.inputCharacters)
             if self.mainFolder in self.mainFolderChoices:
                 break
             else:
                 self.on_error(f'invalid option "{self.mainFolder}"')
+        return
     
     @classmethod
     def print_options(self):
         self.print_command_separator()
-        print(f' {self.addFileCommand} to add folder & file, e.g: "F1/f2/ex.cpp"')
+        print(f' {self.addFileCommand} to add folder & file, example: "F1/f2/ex.cpp"')
         print(f' Some file-extensions have associated files auto-generated, example: "ex.h" gets a "ex.cpp"')
-        print(f' {self.addFileNoExtraCommand} same as {self.addFileCommand} without auto-generated associated file')
+        print(f' {self.addFileSingle} same as {self.addFileCommand} without auto-generation of associated file')
         print(f' {self.undoFileCommand} to undo, example: {self.undoFileCommand} 1')
-        print(f' {self.switchMainDirCommand} to change main direcotry')
-        print(f' {self.continueCommand} to create files')
+        print(f' {self.switchMainCommand} to change main folder')
+        print(f' {self.continueCommand} continue to file generation')
+        return
     
     @classmethod
     def print_status(self):
-            self.print_command_separator()
-            print(f"Current main directory: {self.choiceToPath[self.mainFolder]}")
-            print(f"Files:")
-            for i, (_, file) in enumerate(self.filesToAdd):
-                print(f'+ [{i + 1}] {file}')
+        self.print_command_separator()
+        print(f"Main folder: {self.choiceToPath[self.mainFolder]}")
+        print(f"Files:")
+        for i, (_, file) in enumerate(self.filesToAdd):
+            print(f'+ [{i + 1}] {file}')
+        return
     
     @classmethod
     def try_add_associated_file(self, fileName:str, folderNames:list[str]):
         # based on file extension determine if an additional file should be added
         fileNameSplit = fileName.split('.')
         extension = fileNameSplit[1]
-        checks = [
-            extension.lower() in [
-                'cpp', 
-                'c', 
-                'h', 
-                'hpp', 
-                ],
-        ]
-        if not all(checks):
-            return
-        
         associatedExtension = ""
         match extension:
             case "cpp":
@@ -189,7 +170,7 @@ class FileCreationUtil:
     @classmethod
     def valid_folder(self, folderName:str):
         if ValidationUtil.validate_folder_name(folderName) is False:
-            self.on_error(f'folder contains invalid characters "{folderName}"')
+            self.on_error(f'"{folderName}" contains invalid characters')
             return False     
         return True
     
@@ -197,13 +178,13 @@ class FileCreationUtil:
     def valid_file(self, fileName:str):
         filenameSplit = fileName.split('.')
         if ValidationUtil.validate_file_name(filenameSplit[0]) is False:
-            self.on_error(f'filename contains invalid characters "{fileName}"')
+            self.on_error(f'"{fileName}" contains invalid characters')
             return False
         
         if (len(filenameSplit) == 1 # Missing extension
             or len(filenameSplit) > 2 # More than 1 extension
             or ValidationUtil.validate_file_extension(filenameSplit[1]) is False):
-            self.on_error(f'unsupported extension "{fileName}"')
+            self.on_error(f'unsupported extension in "{fileName}"')
             return False
         
         return True
@@ -302,9 +283,27 @@ class FileCreationUtil:
         for (mainFolder, fileToAdd) in self.filesToAdd:
             self.add_file_to_cmake(mainFolder, fileToAdd)
         print("\nRegenerating project ...")
-        subprocess.call([os.path.abspath("./../ProjectSetup/GenerateProjectFiles.bat"), "nopause"])
+        subprocess.call([os.path.abspath(self.generatorScriptPath), "nopause"])
         self.filesToAdd = []
         return
+
+    @classmethod
+    def try_add_file(self, fileToAdd:str):
+        if fileToAdd == "":
+            return False
+
+        (folderNames, fileName) = self.extract_folders_and_file(fileToAdd)
+        foldersValid = True
+        for filePart in folderNames:
+            if not self.valid_folder(filePart):
+                foldersValid = False
+        if not foldersValid:
+            return False
+        if not self.valid_file(fileName):
+            return False
+                                
+        self.filesToAdd.append((self.mainFolder, self.choiceToPath[self.mainFolder] + fileToAdd))
+        return True
 
     @classmethod
     def process_commands(self):
@@ -312,7 +311,7 @@ class FileCreationUtil:
             self.print_options()
             self.print_status()
 
-            userInput=input(">> ")
+            userInput=input(self.inputCharacters)
 
             if self.continueCommand in userInput:
                 self.generate_and_flush()
@@ -321,42 +320,19 @@ class FileCreationUtil:
 
             if self.addFileCommand in userInput: 
                 fileToAdd = "".join(userInput.replace(f"{self.addFileCommand}", '').split())
-                if fileToAdd == "":
+                if not self.try_add_file(fileToAdd):
                     continue
-
+                
                 (folderNames, fileName) = self.extract_folders_and_file(fileToAdd)
-                foldersValid = True
-                for filePart in folderNames:
-                    if not self.valid_folder(filePart):
-                        foldersValid = False
-                if not foldersValid:
-                    continue
-                if not self.valid_file(fileName):
-                    continue
-                                        
-                self.filesToAdd.append((self.mainFolder, self.choiceToPath[self.mainFolder] + fileToAdd))
                 self.try_add_associated_file(fileName, folderNames)
                 continue
             
-            if self.addFileNoExtraCommand in userInput: 
-                fileToAdd = "".join(userInput.replace(f"{self.addFileNoExtraCommand}", '').split())
-                if fileToAdd == "":
-                    continue
-
-                (folderNames, fileName) = self.extract_folders_and_file(fileToAdd)
-                foldersValid = True
-                for filePart in folderNames:
-                    if not self.valid_folder(filePart):
-                        foldersValid = False
-                if not foldersValid:
-                    continue
-                if not self.valid_file(fileName):
-                    continue
-                                        
-                self.filesToAdd.append((self.mainFolder, self.choiceToPath[self.mainFolder] + fileToAdd))
+            if self.addFileSingle in userInput: 
+                fileToAdd = "".join(userInput.replace(f"{self.addFileSingle}", '').split())
+                self.try_add_file(fileToAdd)
                 continue
             
-            # TODO: figure out how handle multiple indices at once
+            # TODO: nice to have - handle multiple indices at once
             if self.undoFileCommand in userInput:
                 filesToUndo = re.findall('[0-9]+', userInput)
                 if len(filesToUndo) == 0:
@@ -369,19 +345,19 @@ class FileCreationUtil:
                 del self.filesToAdd[undoFileIndex]
                 continue
             
-            if self.switchMainDirCommand in userInput:
-                self.select_main_dir()
-                continue
-            
-    
+            if self.switchMainCommand in userInput:
+                self.select_main_folder()
+                continue 
+        return
+
 if __name__ == "__main__":
     print("** File Creation Utility **")
     print("Instructions:")
-    print(" 1 - Select a main directory (can be changed), any file added will be placed under it")
+    print(" 1 - Select a main folder (can be changed), any file added will be placed under it")
     print(" 2 - Add as many files as you want.")
     print(" 3 - Generate: generation updates CMakeLists and updates the project files")
     print()
     fileCreator = FileCreationUtil()
     while(True):
-        fileCreator.select_main_dir()
+        fileCreator.select_main_folder()
         fileCreator.process_commands()
