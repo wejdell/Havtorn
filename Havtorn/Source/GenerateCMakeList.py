8fe94122748b8d269be73061f5ccc68a578f980e
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-10-10 16:52:10 +0200

    cleanup

--- Havtorn/Source/GenerateCMakeList.py
+++ Havtorn/Source/GenerateCMakeList.py
@@ -1,11 +1,9 @@
 import os
 import re
-from enum import Enum
 from glob import glob
 from collections import defaultdict
 from HavtornMainFoldersUtility import HavtornMainFoldersUtility
 from HavtornMainFoldersUtility import HavtornKeys
-
 
 # Scans Havtorn's Source-folder for files and adds them to CMakeLists
 # External-folders are excluded from scan since we want a handpicked selection from them
@@ -35,7 +33,7 @@ class CMakeTextsGenerator:
             HavtornMainFoldersUtility.get_file_specifier( HavtornKeys.GeometryShaders),
             },
     }
-    # add .hint, .vcxproj.user,
+
     targetSpecificExclusions = {
         HavtornKeys.Engine:{
             'Shaders',
@@ -47,11 +45,14 @@ class CMakeTextsGenerator:
         ".vcxproj.user",
     }
 
-    # put in __init__
-    filesToAdd = defaultdict(list[str])
-
     @classmethod
-    def Test(self):
+    def __init__(self):
+        self.filesToAdd = defaultdict(list[str])
+        return
+    
+    # should also set the values of CMakes set under FOLDERS
+    @classmethod
+    def Run(self, templatePath:str, cmakeListsPath:str):
         for target in self.targets:
             self.filesToAdd.update({target : list[str]()})
             try:
@@ -81,9 +82,6 @@ class CMakeTextsGenerator:
             except Exception as e:
                 print(e)
 
-        templatePath = "CMakeListsTemplate.txt"
-        cmakeListsPath = "CMakeLists.txt"
-
         cmakeFileAsLineList = list[str]
         with open(templatePath, "r") as templateFile: 
             cmakeFileAsLineList = templateFile.readlines()
@@ -98,38 +96,6 @@ class CMakeTextsGenerator:
             cmakeFile.writelines(cmakeFileAsLineList)
         return
 
-    @classmethod
-    def Do(self):
-        # for all main folders
-            # get all [cpp, h, ..etc] 
-        for mainFolder in self.mainFolders:
-            print(mainFolder)
-            try:
-                for file in glob(os.path.abspath(mainFolder) + '/**', recursive=True):
-                    if os.path.isfile(file):
-                        print("\t" + file.split("Source\\")[1])
-            except Exception as e:
-                print(e)
-
-        print(self.externalFolder)
-        try:
-            for file in glob(os.path.abspath(self.externalFolder) + '/**', recursive=True):
-                if os.path.isfile(file):
-                    print("\t ..\\External\\ " + file.split("\\External\\")[1])
-        except Exception as e:
-            print(e)
-            #for file in os.listdir(mainFolder):
-            #    try:
-            #        print("\t" + os.path.abspath(file) + "  " + f"{os.path.isfile(os.path.abspath(file))}")
-            #    except Exception as e:
-            #        print(e)
-
-        while(True):
-            c = input("c to close")
-            if c == "c":
-                break
-        return
-
 if __name__ == "__main__":
     generator = CMakeTextsGenerator()
-    generator.Test()
+    generator.Run("CMakeListsTemplate.txt", "CMakeLists.txt")
