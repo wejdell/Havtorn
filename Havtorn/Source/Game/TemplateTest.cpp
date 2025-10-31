e1307a2ae3f7b48c8be87fe8fd5e5d92431c7958
aki-gonzalez <gonzalez_aki@hotmail.com>
2025-10-31 13:47:08 +0100

    Cleaned up TemplateCreatorUtil

---
+++ Havtorn/Source/Game/TemplateTest.cpp
@@ -0,0 +1,20 @@
+// Havtorn license 2025
+#include "TemplateTest.h"
+
+namespace Havtorn
+{
+        int TemplateTest::Do()
+        {
+            return -1;
+        }
+
+        TemplateTest::TemplateTest()
+        {
+
+        }
+        
+        TemplateTest::~TemplateTest()
+        {
+
+        }
+}
---
+++ Havtorn/Source/Game/TemplateTest.h
@@ -0,0 +1,14 @@
+// Havtorn license 2025
+#pragma once
+#include <vector>
+
+namespace Havtorn
+{
+    struct TemplateTest
+    {
+        int Do();
+        int CustomName();
+        TemplateTest();
+        ~TemplateTest();
+    }
+}
