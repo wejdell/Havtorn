d8195e94d58b0c3b40c47c5eb8b2604cf1417890
Axel Savage <axelsavage@gmail.com>
2025-11-01 12:18:19 +0100

    Re-added CMake  entry for new Nodes

--- Havtorn/Source/Game/GameScript.cpp
+++ Havtorn/Source/Game/GameScript.cpp
@@ -1,11 +1,8 @@
 // Copyright 2025 Team Havtorn. All Rights Reserved.
-
 #include "hvpch.h"
 #include "GameScript.h"
+#include "NodeInclude.h"
 #include <Scene/World.h>
-#include "GhostyNode.h"
-
-#include "Ghosty/SGhostyNodeEditorContext.h"
 
 namespace Havtorn
 {
@@ -22,5 +19,7 @@ namespace Havtorn
 		//typeId = 10000;
 		NodeFactory->RegisterNodeType<HexRune::SGhostyNode, HexRune::SGhostyNodeEditorContext>(this, typeId++);
 		NodeFactory->RegisterNodeType<HexRune::SGhostyPositionNode, HexRune::SGhostyNodePositionEditorContex>(this, typeId++);
+		NodeFactory->RegisterNodeType<HexRune::SInterpolatePosition, HexRune::SInterpolatePositionNodeEditorContext>(this, typeId++);
+		NodeFactory->RegisterNodeType<HexRune::STimerNode, HexRune::STimerNodeEditorContext>(this, typeId++);
 	}
 }
