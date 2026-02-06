e82e9c511a5da239eb840f2b93ac8dcebe087eff
AxelSavage <axelsavage@gmail.com>
2026-02-06 09:52:44 +0100

    Input Asset - New Files

---
+++ Havtorn/Source/Editor/AuthoringTools/InputTool.h
@@ -0,0 +1,37 @@
+// Copyright 2026 Team Havtorn. All Rights Reserved.
+#pragma once
+#include "EditorWindow.h"
+
+namespace Havtorn
+{
+
+	struct SEditorAssetRepresentation;
+
+	enum class EInputDataType 
+	{
+		Button,
+
+	};
+
+	struct SInputToolOperation
+	{
+		EInputDataType InputDataType;
+
+
+
+
+	};
+
+	class CInputTool : public CWindow
+	{
+	public:
+		CInputTool(const char* displayName, CEditorManager* manager);
+		~CInputTool() override = default;
+
+		void OnEnable() override;
+		void OnInspectorGUI() override;
+		void OnDisable() override;
+
+		void OpenInputAsset(SEditorAssetRepresentation* asset);
+	};
+}
