c1c08e2b87e34a1b71e85eda288e227d436b7f0c
AxelSavage <axelsavage@gmail.com>
2026-04-26 11:38:38 +0200

    Implemented GetComponentName in AbilityComponentEditorContext

--- Havtorn/Source/Engine/ECS/ComponentEditorContexts/AbilityComponentEditorContext.h
+++ Havtorn/Source/Engine/ECS/ComponentEditorContexts/AbilityComponentEditorContext.h
@@ -10,6 +10,7 @@ namespace Havtorn
 		SComponentViewResult View(const SEntity& entityOwner, CScene* scene) const override;
 		bool AddComponent(const SEntity& entity, CScene* scene) const override;
 		bool RemoveComponent(const SEntity& entity, CScene* scene) const override;
+		virtual const char* GetComponentName() const { return "Ability"; };
 
 		static SAbilityComponentEditorContext Context;
 	};
