983f79eb55653691815e87db52283e56705b76ce
Nicolas Risberg <nicolas.risberg@gmail.com>
2026-05-22 20:23:16 +0200

    Fixed serialization issue with hex command component

--- Havtorn/Source/Engine/Hex/SHexCommandComponent.cpp
+++ Havtorn/Source/Engine/Hex/SHexCommandComponent.cpp
@@ -8,20 +8,20 @@ namespace Havtorn
 	void SHexCommandComponent::Serialize(char* toData, U64& pointerPosition) const
 	{
 		SerializeData(Owner, toData, pointerPosition);
-		SerializeData(TagsToListenFor, toData, pointerPosition);
+		TagsToListenFor.Serialize(toData, pointerPosition);
 	}
 
 	void SHexCommandComponent::Deserialize(const char* fromData, U64& pointerPosition)
 	{
 		DeserializeData(Owner, fromData, pointerPosition);
-		DeserializeData(TagsToListenFor, fromData, pointerPosition);
+		TagsToListenFor.Deserialize(fromData, pointerPosition);
 	}
 
 	U32 SHexCommandComponent::GetSize() const
 	{
 		U32 size = 0;
 		size += GetDataSize(Owner);
-		size += GetDataSize(TagsToListenFor);
+		size += TagsToListenFor.GetSize();
 
 		return size;
 	}
