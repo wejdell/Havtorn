eecb5c1f5559b455123cdfca179e5d8d582285a4
Nicolas Risberg <nicolas.risberg@gmail.com>
2026-06-24 22:09:57 +0200

    Moved setting active camera logic in CameraSystem to callable functions

--- Havtorn/Source/Game/GameManager.cpp
+++ Havtorn/Source/Game/GameManager.cpp
@@ -80,7 +80,7 @@ namespace Havtorn
 	{
 	}
 
-	void CGameManager::OnBeginPlay(std::vector<Ptr<CScene>>& /*scenes*/)
+	void CGameManager::OnBeginPlay(std::vector<Ptr<CScene>>& scenes)
 	{
 		World->RequestSystem<CInputSystem>(this);
 		World->RequestSystem<CSpriteAnimatorGraphSystem>(this);
@@ -94,6 +94,8 @@ namespace Havtorn
 
 		if (SUICanvasComponent* mainMenuCanvas = World->GetComponent<SUICanvasComponent>(SEntity(CScene::MainMenuEntityGUID)))
 			mainMenuCanvas->IsActive = true;
+
+		World->GetSystem<CCameraSystem>()->SetStartingCamera(scenes);
 	}
 
 	void CGameManager::OnPausePlay(std::vector<Ptr<CScene>>& /*scenes*/)
@@ -103,7 +105,7 @@ namespace Havtorn
 		// TODO.NW: Notify audio of pause state?
 	}
 
-	void CGameManager::OnEndPlay(std::vector<Ptr<CScene>>& /*scenes*/)
+	void CGameManager::OnEndPlay(std::vector<Ptr<CScene>>& scenes)
 	{
 		World->UnrequestSystem<CInputSystem>(this);
 		World->BlockPhysicsSystem(this);
@@ -111,6 +113,8 @@ namespace Havtorn
 
 		if (CUISystem* uiSystem = World->GetSystem<CUISystem>())
 			uiSystem->ClearFocus();
+
+		World->GetSystem<CCameraSystem>()->ResetStartingCamera(scenes);
 	}
 
 	void CGameManager::PlayFromScene(const std::string_view sceneName)
